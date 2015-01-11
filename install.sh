76458fa4bae64134945c52b80913f5b454de0b68
Ryan Burnette <ryan.burnette@gmail.com>
2015-01-11 01:44:19 -0500

    eh

--- install.sh
+++ install.sh
@@ -9,14 +9,12 @@ ln -sf $PWD/.gemrc $HOME/.gemrc
 ln -sf $PWD/.gitconfig $HOME/.gitconfig
 ln -sf $PWD/.gitignore_global $HOME/.gitignore_global
 
-# jshint
-ln -sf $PWD/.jshintrc $HOME/.jshintrc
-
 # vim
 ln -sf $PWD/.vimrc $HOME/.vimrc
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 mkdir -p $HOME/.vim/bundle
 cd $HOME/.vim/bundle && git clone git://github.com/altercation/vim-colors-solarized.git
+cd $HOME/.vim && git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
 
 # fish
 mkdir -p ~/.config/fish
